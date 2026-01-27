c5c0f2b23499fc58f9c94b3e58cddeee3f27c25f
SeanTheBuilder1 <seanthebuilder52@gmail.com>
2026-01-28 05:33:01 +0800

    style: manual pass

--- src/main.py
+++ src/main.py
@@ -3,12 +3,14 @@ import json
 
 from lexer_analyzer import analyzeSource
 
+
 def build_line_starts(text: str):
     line_starts = [0]
     for i, c in enumerate(text):
         if c == "\n":
             line_starts.append(i + 1)
     return line_starts
+
 
 def index_to_line_col_batch(idx: int, line_starts: list[int]):
     low, high = 0, len(line_starts) - 1
@@ -22,6 +24,7 @@ def index_to_line_col_batch(idx: int, line_starts: list[int]):
     col = idx - line_starts[high] + 1
     return line, col
 
+
 def main():
     if len(sys.argv) < 2:
         print("Usage:", sys.argv[0], "[file] [optional: save file]")
@@ -34,22 +37,23 @@ def main():
     for t in tokens:
         start_line, start_col = index_to_line_col_batch(t.start, line_starts)
         end_line, end_col = index_to_line_col_batch(t.end, line_starts)
-        token_list.append({
-            'type': str(t.type.name),
-            'start': int(t.start),
-            'end': int(t.end),
-            'start_line': start_line,
-            'start_col': start_col,
-            'end_line': end_line,
-            'end_col': end_col,
-            'lexeme': code[t.start:t.end],
-        })
+        token_list.append(
+            {
+                "type": str(t.type.name),
+                "start": int(t.start),
+                "end": int(t.end),
+                "start_line": start_line,
+                "start_col": start_col,
+                "end_line": end_line,
+                "end_col": end_col,
+                "lexeme": code[t.start : t.end],
+            }
+        )
     print(tokens)
     file.close()
     if len(sys.argv) >= 3:
         with open(sys.argv[2], "w") as f:
             json.dump(token_list, f, indent=2)
-
 
 
 if __name__ == "__main__":
