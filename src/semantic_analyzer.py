573b15960e41171b68f5e5e9e50494dab84c72d1
SeanTheBuilder1 <seanthebuilder52@gmail.com>
2026-02-14 01:04:35 +0800

    feat: add type priority tables/list
    for operational expressions

--- src/semantic_analyzer.py
+++ src/semantic_analyzer.py
@@ -103,6 +103,78 @@ def resolveFile(tree: ASTNode, code: str) -> tuple[Scope, bool]:
         ModifierClass.ACCELERATION,
     ]
 
+    distance_to_area = {
+        ModifierTypes.METER_TYPE: ModifierTypes.METER2_TYPE,
+        ModifierTypes.MM_TYPE: ModifierTypes.MM2_TYPE,
+        ModifierTypes.CM_TYPE: ModifierTypes.CM2_TYPE,
+        ModifierTypes.KM_TYPE: ModifierTypes.KM2_TYPE,
+        ModifierTypes.FT_TYPE: ModifierTypes.FT2_TYPE,
+        ModifierTypes.INCH_TYPE: ModifierTypes.INCH2_TYPE,
+    }
+
+    distance_or_area_to_volume = {
+        ModifierTypes.METER_TYPE: ModifierTypes.KL_TYPE,
+        ModifierTypes.MM_TYPE: ModifierTypes.ML_TYPE,
+        ModifierTypes.CM_TYPE: ModifierTypes.ML_TYPE,
+        ModifierTypes.KM_TYPE: ModifierTypes.KL_TYPE,
+        ModifierTypes.FT_TYPE: ModifierTypes.KL_TYPE,
+        ModifierTypes.INCH_TYPE: ModifierTypes.KL_TYPE,
+        ModifierTypes.METER2_TYPE: ModifierTypes.KL_TYPE,
+        ModifierTypes.MM2_TYPE: ModifierTypes.ML_TYPE,
+        ModifierTypes.CM2_TYPE: ModifierTypes.ML_TYPE,
+        ModifierTypes.KM2_TYPE: ModifierTypes.KL_TYPE,
+        ModifierTypes.FT2_TYPE: ModifierTypes.KL_TYPE,
+        ModifierTypes.INCH2_TYPE: ModifierTypes.KL_TYPE,
+    }
+
+    area_to_distance = {
+        ModifierTypes.METER2_TYPE: ModifierTypes.METER_TYPE,
+        ModifierTypes.MM2_TYPE: ModifierTypes.MM_TYPE,
+        ModifierTypes.CM2_TYPE: ModifierTypes.CM_TYPE,
+        ModifierTypes.KM2_TYPE: ModifierTypes.KM_TYPE,
+        ModifierTypes.FT2_TYPE: ModifierTypes.FT_TYPE,
+        ModifierTypes.INCH2_TYPE: ModifierTypes.INCH_TYPE,
+    }
+
+    int_promotion_table = {
+        BuiltInTypes.FLOAT_TYPE: {
+            BuiltInTypes.FLOAT_TYPE,
+            BuiltInTypes.INT_TYPE,
+            BuiltInTypes.CHAR_TYPE,
+            BuiltInTypes.BOOL_TYPE,
+        },
+        BuiltInTypes.INT_TYPE: {
+            BuiltInTypes.INT_TYPE,
+            BuiltInTypes.CHAR_TYPE,
+            BuiltInTypes.BOOL_TYPE,
+        },
+        BuiltInTypes.CHAR_TYPE: {BuiltInTypes.CHAR_TYPE, BuiltInTypes.BOOL_TYPE},
+        BuiltInTypes.BOOL_TYPE: {BuiltInTypes.BOOL_TYPE},
+    }
+    int_types = [
+        BuiltInTypes.FLOAT_TYPE,
+        BuiltInTypes.INT_TYPE,
+        BuiltInTypes.CHAR_TYPE,
+        BuiltInTypes.BOOL_TYPE,
+    ]
+    strictly_int_types = [
+        BuiltInTypes.INT_TYPE,
+        BuiltInTypes.CHAR_TYPE,
+        BuiltInTypes.BOOL_TYPE,
+    ]
+    modifier_priority_table = {
+        ModifierClass.PERCENT: percent_types,
+        ModifierClass.TIME: time_types,
+        ModifierClass.DISTANCE: distance_types,
+        ModifierClass.AREA: area_types,
+        ModifierClass.VOLUME: volume_types,
+        ModifierClass.MASS: mass_types,
+        ModifierClass.TEMP: temp_types,
+        ModifierClass.FORCE: force_types,
+        ModifierClass.VELOCITY: velocity_types,
+        ModifierClass.ACCELERATION: accel_types,
+    }
+
     def nonFatalError(*args):
         nonlocal has_error
         print(*args)
